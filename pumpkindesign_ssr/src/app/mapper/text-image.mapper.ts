7ef052a1d093246f7d58a1d6ceee41cdf565ab3b
LittlePumpkin87 <littlepumpkin.jr@googlemail.com>
2026-06-13 10:48:23 +0200

    TEST if this is the cause of production rendering error. nor reproducable on localhost

--- pumpkindesign_ssr/src/app/mapper/text-image.mapper.ts
+++ pumpkindesign_ssr/src/app/mapper/text-image.mapper.ts
@@ -1,9 +1,19 @@
+import { CTA } from '../interfaces/atom.interface';
 import { TextImageItem } from '../interfaces/organism.interface';
 import { getImageUrl, getLinkData, serializeRichText } from '../utils/content-helper';
 
 export const mapTextImageData = (rawData: any): { item: TextImageItem } | undefined => {
   if (!rawData) {
     return undefined;
+  }
+  // Map CTA Array
+  let normalizedCta: any[] = [];
+  if (Array.isArray(rawData.cta)) {
+    normalizedCta = rawData.cta.map((link: CTA) => getLinkData(link)).filter(Boolean);
+  } else if (rawData.cta) {
+    normalizedCta = [getLinkData(rawData.cta)];
+  } else {
+    normalizedCta = [];
   }
 
   return {
@@ -12,7 +22,7 @@ export const mapTextImageData = (rawData: any): { item: TextImageItem } | undefi
       subline: rawData.subline,
       text: serializeRichText(rawData.text),
       image_position: rawData.image_position,
-      cta: getLinkData(rawData.cta),
+      cta: normalizedCta,
       imgSrc: getImageUrl(rawData?.image),
       imgAlt: rawData.image.alternativeText,
     },
